478b18c92b4a6ab0b7dbf92e23fe1ecd78160d25
Eric Zou <ezou626@gmail.com>
2025-03-02 20:06:22 -0500

    add middleware and keepalive

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+# server config
+from asyncio import create_task
+from contextlib import asynccontextmanager
+from server_utils import keep_alive
+from fastapi.middleware.cors import CORSMiddleware
 
+@asynccontextmanager
+def lifespan(app: FastAPI): 
+    create_task(keep_alive)
+    yield
+
+app = FastAPI(lifespan = lifespan)
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins = ['*'], # change to frontend URL 
+    allow_credentials = True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 @app.get("/")
 async def root():
