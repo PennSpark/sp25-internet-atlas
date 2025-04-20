3d386884c822a29ee1bfa5e59b00660a88d3b5ec
Jimin Choi <72880905+saordoe@users.noreply.github.com>
2025-04-19 22:36:36 -0400

    Landmarks (wip)

--- frontend/src/components/Graph3D.tsx
+++ frontend/src/components/Graph3D.tsx
@@ -52,14 +52,39 @@ export default function Graph3D({ data }: Graph3DProps) {
     // Initial size
     resize();
 
+    // counts edges for landmark size
+    const outgoingEdges: Record<string, number> = {};    
+    data.nodes.forEach(node => {
+      outgoingEdges[node.id] = data.links.filter(
+        link => link.source === node.id).length;
+    });
+
     graph
       .graphData(data)
       .nodeAutoColorBy('id')
       .nodeLabel('name')
       .nodeThreeObject((node) => {
+        const group = new THREE.Group();
         const sphereGeometry = new THREE.SphereGeometry(1, 16, 16);
         const sphereMaterial = new THREE.MeshBasicMaterial({ color: node.color || 'orange' });
-        return new THREE.Mesh(sphereGeometry, sphereMaterial);
+        const nodeSphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+        group.add(nodeSphere);
+        
+        // landmarks
+        const connections = outgoingEdges[node.id] || 0;
+        const landmarkRadius = 2 + connections * 3.5; // arbitraruy number to vary size of landmarks based on # of outgoing edges
+        
+        const landmarkGeometry = new THREE.SphereGeometry(landmarkRadius, 32, 32);
+        const landmarkMaterial = new THREE.MeshBasicMaterial({ 
+          color: node.color || 'orange',
+          transparent: true,
+          opacity: 0.1
+        });
+        
+        const landmarkSphere = new THREE.Mesh(landmarkGeometry, landmarkMaterial);
+        group.add(landmarkSphere);
+        
+        return group;
       })
       .nodeThreeObjectExtend(false)
       .linkCurvature('curvature')
