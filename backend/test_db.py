7def937af0a6d57022d94628d1b4634a170deb18
Eric Zou <ezou626@gmail.com>
2025-04-21 03:21:42 -0400

    test that you can access the tb

---
+++ backend/test_db.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from supabase import create_client, Client
+import os
+
+load_dotenv()
+
+# Initialize Supabase client
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_ADMIN_KEY")
+SUPABASE: Client = create_client(url, key)
+
+result = SUPABASE.table("browsing_counts").select("*").limit(10).order('visit_count', desc=True).execute()
+print(result.data[:10])
