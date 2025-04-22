0029e07a89eb07999852c996c76cb11c706136ab
kimestelle <137699667+kimestelle@users.noreply.github.com>
2025-04-22 19:43:47 -0400

    back to browserrouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingScreen from './LandingScreen';
 import NodeGraph from './components/NodeGraph';
 
