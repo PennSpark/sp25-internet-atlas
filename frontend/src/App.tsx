eeb56c6ccb2285dda8a6f57e29e87d923f4d7886
kimestelle <137699667+kimestelle@users.noreply.github.com>
2025-04-22 19:44:51 -0400

    nvm hasrouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingScreen from './LandingScreen';
 import NodeGraph from './components/NodeGraph';
 
